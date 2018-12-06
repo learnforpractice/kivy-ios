70efb35d1b33a88ca42625798952fb65de5e0c21
learnforpractice <learnforpractice@gmail.com>
2018-12-06 15:16:49 +0800

    Fix pyobjus build on mac OS X

--- recipes/pyobjus/__init__.py
+++ recipes/pyobjus/__init__.py
@@ -2,7 +2,8 @@ from toolchain import CythonRecipe
 
 class PyobjusRecipe(CythonRecipe):
     version = "master"
-    url = "https://github.com/kivy/pyobjus/archive/{version}.zip"
+#    url = "https://github.com/kivy/pyobjus/archive/{version}.zip"
+    url = "https://github.com/cruor99/pyobjus/archive/{version}.zip"
     library = "libpyobjus.a"
     depends = ["python"]
     pre_build_ext = True
