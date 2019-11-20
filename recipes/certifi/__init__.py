68f0660ab302588c8334610f6ab302eb9281ae80
learnforpractice <learnforpractice@gmail.com>
2019-11-20 21:33:27 +0800

    Add requests

---
+++ recipes/certifi/__init__.py
@@ -0,0 +1,23 @@
+# pure-python package, this can be removed when we'll support any python package
+from toolchain import PythonRecipe, shprint
+from os.path import join
+import sh, os
+
+class CertifiRecipe(PythonRecipe):
+    version = "2019.09.11"
+    url = "https://github.com/certifi/python-certifi/archive/{version}.zip"
+    depends = ["python3"]
+
+    def install(self):
+        arch = list(self.filtered_archs)[0]
+        build_dir = self.get_build_dir(arch.arch)
+        os.chdir(build_dir)
+        hostpython = sh.Command(self.ctx.hostpython)
+        build_env = arch.get_env()
+        dest_dir = join(self.ctx.dist_dir, "root", "python3")
+        site_packages_path = join(dest_dir, 'lib', 'python3.7', 'site-packages')
+        build_env['PYTHONPATH'] = site_packages_path
+        shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
+
+recipe = CertifiRecipe()
+
---
+++ recipes/chardet/__init__.py
@@ -0,0 +1,23 @@
+# pure-python package, this can be removed when we'll support any python package
+from toolchain import PythonRecipe, shprint
+from os.path import join
+import sh, os
+
+class ChardetRecipe(PythonRecipe):
+    version = "3.0.4"
+    url = "https://github.com/chardet/chardet/archive/{version}.zip"
+    depends = ["python3"]
+
+    def install(self):
+        arch = list(self.filtered_archs)[0]
+        build_dir = self.get_build_dir(arch.arch)
+        os.chdir(build_dir)
+        hostpython = sh.Command(self.ctx.hostpython)
+        build_env = arch.get_env()
+        dest_dir = join(self.ctx.dist_dir, "root", "python3")
+        site_packages_path = join(dest_dir, 'lib', 'python3.7', 'site-packages')
+        build_env['PYTHONPATH'] = site_packages_path
+        shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
+
+recipe = ChardetRecipe()
+
